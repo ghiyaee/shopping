d2d0cc14e2e89fe412678e33c974ab48dcb05661
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-08-06 00:02:51 +0430

    update colors

--- src/components/Shopping.jsx
+++ src/components/Shopping.jsx
@@ -11,7 +11,7 @@ const Shopping = () => {
          return 
       
     } else if(countContext==1) {
-      return <i onClick={() => countDisptach("decrement")} className="fa-solid fa-trash-can"></i>
+      return <i  onClick={() => countDisptach("decrement")} className="fa-solid fa-trash-can trash"></i>
     }
   }
     return (
@@ -24,8 +24,7 @@ const Shopping = () => {
             </span>
             <span onClick={() => countDisptach("increment")}>+</span>
             <span>{countContext}</span>
-            <span> { handel() }
-            </span>
+            { handel() }
           </div>
         </div>
       </div>
