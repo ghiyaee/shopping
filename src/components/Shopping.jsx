d72a13f5401643ef723f7c11836e77fd7127840e
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-08-07 00:26:35 +0430

    add total at state

--- src/components/Shopping.jsx
+++ src/components/Shopping.jsx
@@ -5,15 +5,17 @@ import {ColorContext} from './colorContext'
 import "./style/shoping.css"
 const Shopping = () => {
   const { countContext, countDisptach } = useContext(MyContext);
+
   const { colorChange, disptachChange } = useContext(ColorContext);
-  console.log(colorChange);
+  console.log(countContext.counter);
   const handelActions = () => {
-    if (countContext > 1) {
+    if (countContext.counter > 1) {
+     
       return  <span className="trash" onClick={() => countDisptach("DECREMET")}> - </span>
-    } else if (countContext == 0) {
+    } else if (countContext.counter == 0) {
          return 
       
-    } else if(countContext==1) {
+    } else if(countContext.counter==1) {
       return <i  onClick={() => countDisptach("DECREMET")} className="fa-solid fa-trash-can trash"></i>
     }
   }
@@ -26,15 +28,16 @@ const Shopping = () => {
       <div className="container">
         <div className="content">
           <h1>shopping cart (basket) with useContext and useReducer</h1>
-          <div
-            className={`row ${colorChange}`} >
+          <div className={`row ${colorChange}`}>
+            <i className="fa-solid fa-basket-shopping"></i>
             <span>
               <i className="fa-solid fa-apple-whole"></i>
             </span>
             <span onClick={() => countDisptach("INCREMET")}>+</span>
-            <span>{countContext}</span>
+            <span>{countContext.counter}</span>
             {handelActions()}
           </div>
+          <p>total:{countContext.total}</p>
           <i
             className={` fa-solid fa-arrows-rotate  `}
             onClick={() =>
@@ -42,7 +45,9 @@ const Shopping = () => {
                 ? disptachChange("COLOR_GHANGE")
                 : disptachChange("COLOR_BACK")
             }
-          ></i>
+          >
+            color
+          </i>
         </div>
       </div>
     );
--- src/components/context.js
+++ src/components/context.js
@@ -1,11 +1,26 @@
 import { createContext,useReducer } from "react";
-const initailState = 0 ;
+const initailState = {
+    counter: 0,
+    total :0
+} ;
 const reducer = (state, action) =>{
     switch (action) {
         case 'INCREMET':
-            return state + 1
+            return {
+                ...state,
+                counter: state.counter + 1,
+                total: state.total + 1
+            }
+     
         case 'DECREMET':
-            return   state > 0 ? state - 1: state = 0
+            return {
+                ...state,
+                counter:
+                    state.counter > 0 ? state.counter - 1 : state.counter = 0,
+                total:
+                state.total > 0 ? state.total - 1 : state.total = 0
+            }
+        
         default:
             return state  
     }
