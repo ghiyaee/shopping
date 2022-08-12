ab652b356e8449f5b2bdc58471be7b9ea4d156c6
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-08-12 13:38:45 +0430

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 // import { MyProvider } from "../src/components/context";
 // import { ColorProvider } from "../src/components/colorContext";
 import { SliderProvider } from "./components/contextSlider";
-
-import Slider from "./components/Slider";
-import Cart from "../src/components/Cart";
 import "../src/components/style/app.css"
-import img1 from "../src/img/img-1.jpg"
-import img2 from "../src/img/img-2.jpg"
-import img3 from "../src/img/img-3.jpg"
-import img4 from "../src/img/img-4.jpg"
 import SliderBar from "./components/Slider";
 const App = () => {
-  const img = [
-    img1,
-    img2,
-    img3,
-    img4
-  ]
-
   return ( 
     <SliderProvider>
     <div className="container">
